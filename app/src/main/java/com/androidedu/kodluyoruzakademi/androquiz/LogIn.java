fa27e31d3fc30cffd9b133381b8058906511a0b7
gamzeuslu <gamze.uslu@yandex.com>
2017-08-09 14:17:44 +0300

    sync

--- app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/LogIn.java
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/LogIn.java
@@ -3,6 +3,10 @@ package com.androidedu.kodluyoruzakademi.androquiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/*
+* First Activity class where users log-in to the quiz.
+*/
+
 public class LogIn extends AppCompatActivity {
 
     @Override
