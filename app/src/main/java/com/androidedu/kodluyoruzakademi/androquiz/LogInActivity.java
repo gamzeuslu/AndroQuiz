7e8b4c013b709b4f52b663689ea9224feef88817
gamzeuslu <gamze.uslu@yandex.com>
2017-08-09 17:48:11 +0300

    Sync

---
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/LogInActivity.java
@@ -0,0 +1,49 @@
+package com.androidedu.kodluyoruzakademi.androquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+* First Activity class where users log-in to the quiz.
+*/
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+}
