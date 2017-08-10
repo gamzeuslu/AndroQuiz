4bdf9455b81abe1bc269a9ea601d148f92712dad
gamzeuslu <gamze.uslu@yandex.com>
2017-08-10 13:39:42 +0300

    Sync

---
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/QuestionsActivity.java
@@ -0,0 +1,13 @@
+package com.androidedu.kodluyoruzakademi.androquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+}
