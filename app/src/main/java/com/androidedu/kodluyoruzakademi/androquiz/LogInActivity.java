d4d46cc0d2395f4bc108db8ec67d84eb11e55532
gamzeuslu <gamze.uslu@yandex.com>
2017-08-11 01:24:04 +0300

    Sync

--- app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/LogInActivity.java
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/LogInActivity.java
@@ -1,7 +1,11 @@
 package com.androidedu.kodluyoruzakademi.androquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /*
 * First Activity class where users log-in to the quiz.
@@ -13,6 +17,27 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        final EditText userNameEntered = (EditText) findViewById(R.id.usernameEditText);
+        Button clickLogInButton = (Button) findViewById(R.id.logInButton);
+
+
+        clickLogInButton.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent intentUserName = new Intent(LogInActivity.this, QuestionsActivity.class);
+                        intentUserName.putExtra("usernameSet", userNameEntered.getText().toString());
+                        startActivity(intentUserName);
+
+                        Intent intentButtonClick = new Intent(LogInActivity.this, QuestionsActivity.class);
+                        startActivity(intentButtonClick);
+                    }
+                }
+        );
+
     }
 
     @Override
@@ -22,26 +47,31 @@ public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+
         super.onStart();
     }
 
     @Override
     protected void onResume() {
+
         super.onResume();
     }
 
     @Override
     protected void onPause() {
+
         super.onPause();
     }
 
     @Override
     protected void onStop() {
+
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
+
         super.onDestroy();
     }
 
--- app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/QuestionsActivity.java
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/androquiz/QuestionsActivity.java
@@ -1,13 +1,63 @@
 package com.androidedu.kodluyoruzakademi.androquiz;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class QuestionsActivity extends AppCompatActivity {
+import static com.androidedu.kodluyoruzakademi.androquiz.R.id.usernameRetrieved;
+
+
+public class QuestionsActivity extends AppCompatActivity implements QuestionFragment.OnFragmentInteractionListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        TextView view = (TextView) findViewById(usernameRetrieved);
+        view.setText("Kullanıcı: " + getIntent().getExtras().getString("usernameSet"));
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
